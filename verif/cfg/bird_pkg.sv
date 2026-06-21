cf8790124de65e1112913e934b6ef8852f540b52
Raghad Murad <raghadmbuzia@gmail.com>
2026-06-21 18:29:34 +0300

    Add base verification infrastructure (package)

--- verif/cfg/bird_pkg.sv
+++ verif/cfg/bird_pkg.sv
@@ -1,8 +1,7 @@
-// ============================================================================
-// bird_pkg.sv - Package with all TB includes in dependency order
-// ============================================================================
 `ifndef BIRD_PKG_SV
 `define BIRD_PKG_SV
+
+// Package with all testbench includes in dependency order
 
 package bird_pkg;
 
@@ -45,7 +44,10 @@ package bird_pkg;
     `include "verif/tests/interleaved_test.sv"
     `include "verif/tests/sampling_test.sv"
     `include "verif/tests/handshake_test.sv"
+    `include "verif/tests/seq_frag_test.sv"
+    `include "verif/tests/reorder_test.sv"
+    `include "verif/tests/cfg_test.sv"
 
 endpackage : bird_pkg
 
-`endif // BIRD_PKG_SV
+`endif
